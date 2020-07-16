09b0b6aa409f69ae83172856dc134a9978ff1ce1
HUGO <hfief1806@gmail.com>
2020-07-17 00:57:54 +0200

    coorection optimisation load image prod

--- views/index.view.php
+++ views/index.view.php
@@ -1,22 +1,32 @@
+<style>
+  body {
+    background: none;
+  }
+</style>
+
 <div class="homepage">
-  <img src="ressources/volley-home.png" width="1519" height="722" alt="page d'accueil">
-  <a href="resultats.php" class="btn btn-primary">
-      Accéder aux résultats
-  </a>
-</div>
+    <img src="ressources/volley-home.png" width="1519" height="722" 
+      alt="page d'accueil">
+  </div>
+
+<a href="resultats.php" class="btn btn-primary res-access">
+  Accéder aux résultats
+</a>
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" 
 	integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
 </script>
 <script>
-  $('.homepage img').first().hide();
+  $( document ).ready(function() {
+    $('.homepage img').first().hide();
 
-  $(function() {
-    setTimeout(function() {
-      $('.homepage img').first().show('slow', function showNextOne() {
-        $(this).next('.homepage img').show('slow', showNextOne);
-      });    
-    }), (3000);
+    $(function() {
+      setTimeout(function() {
+        $('.homepage img').first().show('slow', function showNextOne() {
+          $(this).next('.homepage img').show('slow', showNextOne);
+        });    
+      }), (3000);
+    });
   });
 </script>
 
