d8b36d8fa6155b388f2f87cb42845a28655eda97
HUGO <hfief1806@gmail.com>
2020-08-10 18:27:14 +0200

    correction edition résultats => moins limité

--- crud/views/edit.view.php
+++ crud/views/edit.view.php
@@ -86,6 +86,45 @@
                         </td>
                     </tr>
 
+                    <tr>
+                        <td><label style="width: inherit; padding: 0;">Matchs</label></td>
+                        <td>
+                            <input class="form-control txtField" id="validationServer01" 
+                                type="number" placeholder="Moyenne E4" step="1" 
+                                name="nb_matchs" min="0" max="9999" 
+                                value="<?php echo $value['nb_matchs']; ?>"
+                            >
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td><label style="width: inherit; padding: 0;">Année</label></td>
+                        <td>
+                            <input class="form-control txtField" id="validationServer01" 
+                                type="number" placeholder="Moyenne E4" step="1" 
+                                name="annee" min="2000" max="9999" 
+                                value="<?php echo $value['annee']; ?>"
+                            >
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td style="text-align: right;">
+                            Id Equipe
+                            <select name="equipe_id">
+                                <?php
+                                // on boucle afin de récupérer toutes les annee 
+                                // afin de proposer à l'utilisateur de choisir l'annee
+                                foreach (Bdd::getInstance()->conn->query('SELECT * FROM volley_resultats') as $row) {
+                                    echo '<option value="' . $row['id'] . '">' . 
+                                        $row['id_equipe'] . '</option>';;
+                                }
+                                ?>
+                            </select>
+                        </td>
+                    </tr>
+
             <?php } ?>
 
                 <tr>
