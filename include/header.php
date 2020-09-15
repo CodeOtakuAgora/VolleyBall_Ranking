a8aaa43932720c54cf36d462bc9cb7ed15169351
HUGO <hfief1806@gmail.com>
2020-09-15 17:28:15 +0200

    correction alignement éléments header

--- include/header.php
+++ include/header.php
@@ -13,12 +13,12 @@
     <?php if (isset($titleAdminCrud)) { ?>
     <a style="text-decoration: none; color: #000000; display: flex;
                     margin: 0" href="../index.php">
-        <img width="617" height="768" src="../ressources/favicon-origin.jpg" class="d-inline-block align-top"
+        <img width="617" height="768" src="../ressources/favicon-origin.jpg" class="align-top"
              style="width: 40px; height: 50px;" alt="">
         <?php } else { ?>
         <a style="text-decoration: none; color: #000000; display: flex;
                     margin: 0" href="index.php">
-            <img width="617" height="768" src="ressources/favicon-origin.jpg" class="d-inline-block align-top"
+            <img width="617" height="768" src="ressources/favicon-origin.jpg" class="align-top"
                  style="width: 40px; height: 50px;" alt="logo fsgt 78">
             <?php } ?>
             <h1 style="display: flex; align-items: center; font-size: 20px; font-weight: bold;
